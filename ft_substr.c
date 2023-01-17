94ac5dc950a86a0043f7b57aef9451425e618caa
fvonsovs <filipvon@protonmail.com>
2023-01-17 17:54:02 +0100

    Fixed ft_substr.c undefined behavior if start>len

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 13:39:44 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/01/13 15:21:39 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/01/17 17:47:24 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,18 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*a;
+	size_t	slen;
 
 	if (!s)
-		return (0);
+		return (NULL);
+	slen = ft_strlen((char *)s);
+	if (start >= slen)
+		return (ft_strdup(""));
+	if (len > slen - start)
+		len = slen - start;
 	a = malloc(sizeof(char) * (len + 1));
+	if (!a)
+		return (NULL);
 	ft_strlcpy(a, ((char *) s + start), (len + 1));
 	return (a);
 }
